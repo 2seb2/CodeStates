375063987a7d9e4de2e82ca052c56d4448edfb17
2seb2 <yuo231@naver.com>
2022-10-18 17:10:27 +0900

    json-server 활용 구현 중

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Template from './components/Template';
 import TodoList from './components/TodoList';
 import TodoInsert from './components/TodoInsert';
 import { RiAddCircleFill } from 'react-icons/ri';
+import useFetch from "./util/useFetch";
 
 let nextId = 0;
 
 function App() {
   const [selectedTodo, setSelectedTodo] = useState(null);
   const [insertToggle, setInsertToggle] = useState(false);
-  const [todos, setTodos] = useState([
-    // {
-    //   id: 1,
-    //   text: "할 일 1",
-    //   checked: true,
-    // },
-    // {
-    //   id: 2,
-    //   text: "할 일 2",
-    //   checked: false,
-    // },
-    // {
-    //   id: 3,
-    //   text: "할 일 3",
-    //   checked: true,
-    // },
-  ]);
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(()=> {
+    fetch('http://localhost:3001/todos',
+            {method:'GET',headers:{'Content-Type':'application/json'},})
+          .then(res=>res.json())
+          .then(data=> setTodos(data))
+          .catch(error => console.error('Error:', error));
+  }, [])
+
+  nextId = useFetch('http://localhost:3001/todos').length;
 
   const onInsertToggle = () => {  // 팝업 닫고 열기 (불린값을 이용해)
     if (selectedTodo) {
@@ -46,17 +41,44 @@ function App() {
         checked: false,
       }
 
-      setTodos(todos => todos.concat(todo));
+      // setTodos(todos => todos.concat(todo));
+      fetch(`http://localhost:3001/todos`,{
+        method:'POST', 
+        headers:{'Content-Type':'application/json'}, 
+        body: JSON.stringify(todo)
+      })
+      .catch(error => console.error('Error:', error));
+      
       nextId++;
     }
   }
+
 
   const onCheckToggle = (id) => { // 할 일 체크 변환
     setTodos(todos => 
       todos.map(todo => 
         (todo.id === id ? {...todo, checked: !todo.checked} : todo)
       )
-    )
+    )   
+    // const {
+    //   data
+    // } = fetch(`http://localhost:3001/todos/${id}`, { 
+    //   method:'GET',headers:{'Content-Type':'application/json'},
+    // })
+    // .catch(error => console.error('Error:', error))
+
+    // const putData={
+    //   id: nextId,
+    //   text: todos[id+1].text,
+    //   checked: !todos[id+1].checked,
+    // }
+
+    // fetch(`http://localhost:3001/todos/${id}`, {
+    //   method : "PUT",
+    //   headers: { "Content-type": "application/json" },
+    //   body : JSON.stringify(putData)
+    // })
+    // .catch(error => console.error('Error:', error));
   }
 
   const onChangeSelectedTodo = (todo) => {  // 할 일 선택
@@ -65,7 +87,10 @@ function App() {
 
   const onRemove = (id) => {  // 할 일 삭제
     onInsertToggle();
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    // setTodos(todos => todos.filter(todo => todo.id !== id));
+    fetch(`http://localhost:3001/todos/${id}`,
+      {method:'DELETE',headers:{'Content-Type':'application/json'},})
+      .catch(error => console.error('Error:', error))
   }
 
   const onUpdate = (id, text) => {  // 할 일 업데이트
